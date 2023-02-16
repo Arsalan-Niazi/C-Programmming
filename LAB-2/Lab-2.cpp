8d1dba5897de6346e479c0cac983da14abd4a7bf
Arsalan Niazi <91112959+Arsalan-Niazi@users.noreply.github.com>
2023-02-17 04:28:53 +0500

    Add files via upload

---
+++ LAB-2/Lab-2.cpp
@@ -0,0 +1,10 @@
+#include iostream
+using namespace std;
+int main(){
+here is my first practise of C++ Programming
+coutHello Code Blocksn;
+coutThis is My First C++ Programn;
+coutI am Eager to become a C++ Programmern;
+return 0 means that program is executed successfully
+return 0;
+}
